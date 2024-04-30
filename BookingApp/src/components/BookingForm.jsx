54779b68d20bcf91ee7abaae7b19fb7ed674e11b
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-04-30 23:57:41 +0500

    booking form updated

--- BookingApp/src/components/BookingForm.jsx
+++ BookingApp/src/components/BookingForm.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import '../styles/bookingform.css'
+import Nav from './nav' 
 function BookingForm() {
   return (
     <div className="bookingform">
+      <Nav />
       <div>BookingForm</div>
       {/* title  */}
       <div className='title'>
-      <label htmlFor="title" className='title'>Title:</label>
+      <label htmlFor="title" >Title</label>
+      <input type="text" />
+      </div>
+      {/* url  */}
+      <div className="url">
+      <label htmlFor="Url">URL</label>
       <input type="text" />
       </div>
       {/* description  */}
       <div className='description'                                                                           >
       <label htmlFor="description">Description</label>
-      <input type="text" className='description' />
+      <input type="text" />
       </div>
     </div>
   )
