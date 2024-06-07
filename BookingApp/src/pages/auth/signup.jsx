cf424903324aee4bf30a6d4fe0126a8d3582d02e
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-06-07 21:03:05 +0500

    form validation

--- BookingApp/src/pages/auth/signup.jsx
+++ BookingApp/src/pages/auth/signup.jsx
@@ -1,15 +1,42 @@
 import React,{useState} from "react"
 import './signup.css'
 import logo from '../../assets/logo.png';
-// import {createUser} from '../../Services/UserServices'
+import axios from "axios";
+import Swal from "sweetalert2";
 function Signup(){
+  
   const[isShow, setShow] = useState(false);
   const[isShowConfirmPass, setShowConfirmPass] = useState(false);
 
+  // form validation 
+  const initialValues = {firstname :"", lastname : "", email : "", password : "", confirm_password : ""}
+  const [formValues, setFormValues] = useState(initialValues);
+  const handleChanges = (e)=>{
+    const {name, value} = e.target;
+    setFormValues({...formValues, [name]:value});
+    console.log(formValues);
+  }
   const submitData = (event)=>{
     event.preventDefault();
-    console.log(event)
+    const firstname = event.target.firstname.value;
+    const lastname = event.target.lastname.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const data = {firstname, lastname, email, password}
+    axios.post('http://localhost:8000/users/create',data)
+    .then(res => res,
+      event.target.reset(),
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "You are registered! Please login",
+        showConfirmButton: false,
+        timer: 1500
+      })
+    )
+    .catch(err => console.log(err));
   }
+  // console.log(error)
 
   return (
     <form action="" method="POST" className="signupform" onSubmit={submitData}>
@@ -19,25 +46,25 @@ function Signup(){
     {/* firstname  */}
     <div className="firstnamediv">
     <label htmlFor="">First name*</label>
-    <input className="nameinput" type="text" placeholder="first name" name="firstname" />
+    <input className="nameinput" type="text" placeholder="first name" name="firstname" value ={formValues.firstname} onChange={handleChanges}/>
     </div>
     {/* lastname  */}
     <div className="firstnamediv">
     <label htmlFor="" className="lastnamelabel">last name*</label>
-    <input className="nameinput" type="text" placeholder="last name" name="lastname"/>
+    <input className="nameinput" type="text" placeholder="last name" name="lastname"  value ={formValues.lastname} onChange={handleChanges}/>
     </div>
     </div>
     {/* email  */}
     <div className="Labeldiv">
     <label htmlFor="">Email*</label>
     </div>
-    <input type="text" placeholder="Enter your email" name="email"/>
+    <input type="text" placeholder="Enter your email" name="email"  value ={formValues.email}/>
     {/* password  */}
     <div className="Labeldiv">
     <label htmlFor="">Password*</label>
     </div>
     <div className="password">
-      <input type={isShow?"text":"password"} className='passwordinput' placeholder="Create a password" name="password"/>
+      <input type={isShow?"text":"password"} className='passwordinput' placeholder="Create a password" name="password"  value ={formValues.password} onChange={handleChanges}/>
       {isShow
       ?
       <i class="fa-regular fa-eye-slash eyeicon" onClick={()=>setShow(false)}></i>
@@ -50,7 +77,7 @@ function Signup(){
     <label htmlFor="">Confirm password*</label>
     </div>
     <div className="password">
-      <input type={isShowConfirmPass?"text":"password"} className='passwordinput' placeholder="Password" />
+      <input type={isShowConfirmPass?"text":"password"} className='passwordinput' placeholder="Password" value ={formValues.password} onChange={handleChanges}/>
       {isShowConfirmPass
       ?
       <i class="fa-regular fa-eye-slash eyeicon" onClick={()=>setShowConfirmPass(false)}></i>
@@ -59,7 +86,7 @@ function Signup(){
     }
       </div>
     
-    <button className="signupbtn" onClick={""}> Sign up</button>
+    <button className="signupbtn" value = "submit" > Sign up</button>
     {/*  hrdiv  */}
     <div className="hrdiv">
      <div className="hr"></div>
