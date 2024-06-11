6c28f39c889c34b3eb8a54e43cd63411d5e1322a
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-06-11 22:28:25 +0500

    updated

--- BookingApp/src/pages/auth/login.jsx
+++ BookingApp/src/pages/auth/login.jsx
@@ -1,11 +1,32 @@
 import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.png'  
+import axios from "axios";
+import Swal from 'sweetalert2';
 function Login() {
   const[isShow, setShow] = useState(false);
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const data = {email,password}
+    axios.post('http://localhost:8000/users/login', data)
+        .then(res => {
+          console.log(res);
+          e.target.reset();
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "you are logged in successfully",
+            showConfirmButton: false,
+            timer: 1500
+          });
+        })
+        .catch(err => console.log(err));
+  }
 
   return (
-    <form action="#" className="signupform">
+    <form action="#" method="POST" onSubmit={handleSubmit} className="signupform">
           <img src={logo} alt="logo" className='logologin' />
           <h1>Log in to Your Account</h1>
           <p className="welcometxt">Welcome back to techVision, Good to see you again!</p>
@@ -13,13 +34,13 @@ function Login() {
           <div className="Labeldiv">
           <label htmlFor="">Email*</label>
           </div>
-          <input type="text" placeholder="Enter your email" />
+          <input type="text" placeholder="Enter your email" name='email'/>
           {/* password  */}
           <div className="Labeldiv">
           <label htmlFor="">Password*</label>
           </div>
           <div className="password">
-      <input type={isShow?"text":"password"} className='passwordinput' placeholder="Enter password" />
+      <input type={isShow?"text":"password"} className='passwordinput' placeholder="Enter password" name='password'/>
       {isShow
       ?
       <i class="fa-regular fa-eye-slash eyeicon" onClick={()=>setShow(false)}></i>
@@ -27,7 +48,7 @@ function Login() {
       <i className="fa-regular fa-eye eyeicon" onClick={()=>setShow(true)}></i>
       }
     </div>
-          <button className="signupbtn"> Log in</button>
+          <button className="signupbtn" value="submit" > Log in</button>
           {/*  hrdiv  */}
     <div className="hrdiv">
      <div className="hr"></div>
